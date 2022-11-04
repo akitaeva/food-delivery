863312d7eaf2fa7b0de5c985f3f08122839dbaae
Alina Kitaeva <akitaeva@gmail.com>
2022-11-04 17:41:41 -0400

    adds cart context

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import { Fragment, useState } from "react";
+import { useState } from 'react';
 
-import Header from "./components/Layout/Header";
-import Meals from "./components/Meals/Meals";
-import Cart from "./components/Cart/Cart";
+import CartProvider from './store/CartProvider';
+
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
 
 function App() {
   const [cartIsOpen, setCartIsOpen] = useState(false);
@@ -16,13 +18,13 @@ function App() {
   };
 
   return (
-    <Fragment>
+    <CartProvider>
       <Header onShowCart={showCartHandler} />
       {cartIsOpen && <Cart onClose={hideCartHandler} />}
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
